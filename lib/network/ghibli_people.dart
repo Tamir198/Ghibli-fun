c1bf1ba19fe9a99f20851b7f6a6dbc178d2cb1ba
Tamir198 <1tamir198@gmail.com>
2021-01-09 01:31:08 +0200

    Small test

--- lib/network/ghibli_people.dart
+++ lib/network/ghibli_people.dart
@@ -15,7 +15,7 @@ class GhibliPeople{
     List<dynamic> jsonLIst = json.decode(response.body) as List;
 
     for(final person in jsonLIst){
-      //print(person['name']);
+      print(person['name']);
       peopleList.add(PeopleModel(
         name: person['name'].toString(),
         gender: person['gender'].toString(),
